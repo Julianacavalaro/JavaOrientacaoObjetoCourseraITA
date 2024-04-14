d008921c3a5c900b471bf1c3829bbc733540d4c5
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2024-04-14 11:47:44 -0300

    Ajuste p aula POO Banco

--- Banco/src/ContaCorrente.java
+++ Banco/src/ContaCorrente.java
@@ -2,8 +2,13 @@
 public class ContaCorrente {
 	int saldo;
 	
-	public void sacar(int valor) {
-		saldo= saldo - valor;
+	public int sacar(int valor) {
+		if(valor < saldo) {
+			return 0;
+		}else
+			saldo = saldo - valor;
+		return valor;
+
 	}
 	
 	public void depositar(int valor) {
